2258e98abf6f7784d2e32f2509035097b90b2a25
eduramme <eduramme@gmail.com>
2025-01-21 15:56:19 -0300

    add website title and favicon

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -4,10 +4,20 @@ import { ThemeProvider } from '~~/components/ThemeProvider';
 import '~~/styles/globals.css';
 import { getMetadata } from '~~/utils/scaffold-eth/getMetadata';
 
-export const metadata = getMetadata({
-  title: 'Scaffold-ETH 2 App',
-  description: 'Built with 🏗 Scaffold-ETH 2',
-});
+export const metadata = {
+  ...getMetadata({
+    title: 'ENS Rent',
+    description: 'Rent ENS domains',
+  }),
+  icons: {
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌀</text></svg>',
+        type: 'image/svg+xml',
+      },
+    ],
+  },
+};
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -1,14 +1,14 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : `http://localhost:${process.env.PORT || 3000}`;
-const titleTemplate = "%s | Scaffold-ETH 2";
+const titleTemplate = '%s | Scaffold-ETH 2';
 
 export const getMetadata = ({
   title,
   description,
-  imageRelativePath = "/thumbnail.jpg",
+  imageRelativePath = '/thumbnail.jpg',
 }: {
   title: string;
   description: string;
@@ -42,9 +42,6 @@ export const getMetadata = ({
       },
       description: description,
       images: [imageUrl],
-    },
-    icons: {
-      icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
     },
   };
 };
