9af7661687e953438be5b8424d70640a9d3b2645
eduramme <eduramme@gmail.com>
2025-03-20 13:58:09 -0300

    fix: formatEther price per second

--- packages/nextjs/app/lend/page.tsx
+++ packages/nextjs/app/lend/page.tsx
@@ -322,7 +322,9 @@ function LendPage() {
                 {!!price && (
                   <p className="text-sm text-gray-500 mt-1">
                     Price per second:{' '}
-                    <EthToUsdValue ethAmount={Number(pricePerSecond)} />
+                    <EthToUsdValue
+                      ethAmount={Number(formatEther(pricePerSecond))}
+                    />
                   </p>
                 )}
               </div>
